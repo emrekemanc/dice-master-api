80497d8b8c34ea22b8042d77c7a25c84912b0a20
Muhammet Emre Kemancı <k.emre.k25@gmail.com>
2025-01-30 13:11:44 +0300

    new chancing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Status } from '@prisma/client';
+import { first } from 'rxjs';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(protected userService: UserService){}
+
+
+    @Post('create')
+    async createUser(@Body() body:{
+        firstName: string;
+        lastName: string;
+        userName: string;
+        mail: string;
+        birthDate: Date;
+        msisdn: string;
+        status: Status;
+
+    }){
+        return this.userService.createUser(
+            body.firstName,
+            body.lastName,
+            body.userName,
+            body.mail,
+            body.birthDate,
+            body.msisdn,
+            body.status || Status.ACTIVE
+        )
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Status } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(protected prismaService: PrismaService){}
+
+    async createUser(
+        firstName: string,
+        lastName: string,
+        userName: string,
+        mail: string,
+        birthDate: Date,
+        msisdn: string,
+        status: Status = Status.ACTIVE
+    ){
+        return this.prismaService.user.create({
+            data: {
+                firstName,
+                lastName,
+                userName,
+                mail,
+                birthDate,
+                msisdn,
+                status,
+            },
+        });
+    }
+
+    async getUserById(userId: string){
+        const user = await this.prismaService.user.findUnique({
+            where: {id: userId}
+        })
+        if(!user){
+            throw new Error("User Not Found")
+        }
+        return user
+    }
+    async getUserByUserName(userName: string){
+        const user = await this.prismaService.user.findUnique({
+            where: {userName: userName}
+        })
+    }
+
+}
